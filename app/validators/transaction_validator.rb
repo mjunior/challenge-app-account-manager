104a8f5ad473babf0b3b7ddd94e415348d7e88bf
mjunior <mauriciojs.junior@gmail.com>
2017-08-31 23:54:19 +0000

    Permitindo transações apenas para as contas ativas.

--- app/validators/transaction_validator.rb
+++ app/validators/transaction_validator.rb
@@ -1,18 +1,22 @@
 class TransactionValidator < ActiveModel::Validator  
   def validate(record)
-    if record.transfer?
-      #A conta de destino deve ser do tipo 'Branch' e ser filha de origem.
-      unless record.destination.branch? && !record.origin.branches.detect{|x| x == record.destination}.nil?
-        record.errors.add :destination, "Destination account isn't a branch of origin or is a MAIN account"
+    if record.destination.active?
+      if record.transfer?
+        #A conta de destino deve ser do tipo 'Branch' e ser filha de origem.
+        unless record.destination.branch? && !record.origin.branches.detect{|x| x == record.destination}.nil?
+          record.errors.add :destination, "Destination account isn't a branch of origin or is a MAIN account"
+        end
+      elsif record.contribution?
+        #Aportes são relizados apenas para as matrizes e devem possuir token
+        unless record.destination.main?
+          record.errors.add :destination, "Destination account isn't a MAIN account"
+        end
+        if record.token.blank?
+          record.errors.add :token, "Can't be blank"
+        end
       end
-    elsif record.contribution?
-      #Aportes são relizados apenas para as matrizes e devem possuir token
-      unless record.destination.main?
-        record.errors.add :destination, "Destination account isn't a MAIN account"
-      end
-      if record.token.blank?
-        record.errors.add :token, "Can't be blank"
-      end
+    else
+      record.errors.add :destination, "Destination account isn't active"
     end
   end
 end
