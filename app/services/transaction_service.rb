635976093b6fb613457054f1f0b603bde93b5905
mjunior <mauriciojs.junior@gmail.com>
2017-08-31 23:36:42 +0000

    Removido Service de Transações

--- app/services/transaction_service.rb
+++
@@ -1,9 +0,0 @@
-class TransactionService
-  def self.transf origin, destination, amount
-    if destination.branch? && !origin.children.detect{|x| x == destination}.nil? 
-      #TODO- PROSSEGUIR
-    else
-      #TODO- BLOQUEAR
-    end
-  end
-end
