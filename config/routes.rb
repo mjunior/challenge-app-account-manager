6e811ba54f0f4353aa283ff9145d99359a6c5d97
mjunior <mauriciojs.junior@gmail.com>
2017-09-01 22:20:12 +0000

    Correção da nomemclatura do Controller Transaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'transaction#index'
-
-  resources :transaction
+  root 'transactions#index'
+  
+  resources :transactions
 
   scope '/api', defaults: { format: :json } do
-    resources :transaction
+    resources :transactions
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
